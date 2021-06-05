5e01d901b123f292dbb807098e482291026f8ff4
silviapalves <silviapalves@yahoo.com.br>
2021-06-05 19:58:34 -0300

    organization

--- Smarket.py
+++ Smarket.py
@@ -8,15 +8,17 @@ from sklearn.metrics import confusion_matrix, classification_report
 smarket = pd.read_csv(r"C:\Users\spalves\Desktop\Silvia\Pessoal\ITAU\ALL+CSV+FILES\ALL CSV FILES\Smarket.csv", index_col=0, parse_dates=True)
 smarket.dropna() 
 
+print("\n----------------------------------------------------------------")
+print("\nLogistic Regression\n")
+
+# print(smarket.columns)
+# print(smarket.shape)
+# print(smarket.describe())
+# print(smarket.corr())
+# smarket.Volume.plot()
 
 
-print(smarket.columns)
-print(smarket.shape)
-print(smarket.describe())
-print(smarket.corr())
-
-smarket.Volume.plot()
-
+print("\n------------------All lag and data-------------------------\n")
 formula = 'Direction ~ Lag1+Lag2+Lag3+Lag4+Lag5+Volume'
 model = smf.glm(formula = formula, data=smarket, family=sm.families.Binomial())
 result = model.fit()
@@ -31,12 +33,15 @@ print("Dependent variables")
 print(result.model.endog_names)
 
 predictions = result.predict()
-print(predictions[0:10])
+# print(predictions[0:10])
 
 print(np.column_stack(smarket[["Direction"]]).flatten(), result.model.endog)
 predictions_nominal = [ "Up" if x < 0.5 else "Down" for x in predictions]
 print(confusion_matrix(smarket["Direction"], predictions_nominal))
 print(classification_report(smarket["Direction"],predictions_nominal,digits = 3)) 
+
+
+print("\n------------------All lag and training data-------------------------\n")
 
 x_train = smarket[:'2004'][:]
 y_train = smarket[:'2004']['Direction']
@@ -51,6 +56,8 @@ predictions_train = result_train.predict(x_test)
 predictions_nominal = [ "Up" if x < 0.5 else "Down" for x in predictions_train]
 print(classification_report(y_test, predictions_nominal,digits = 3))
 
+print("\n------------------Lag1 and lag2 and training data-------------------------\n")
+
 formula = 'Direction ~ Lag1+Lag2'
 model_train = smf.glm(formula = formula,data = x_train,family = sm.families.Binomial())
 result_train = model_train.fit()
@@ -60,9 +67,14 @@ predictions_nominal = [ "Up" if x < 0.5 else "Down" for x in predictions_train]
 print(classification_report(y_test, predictions_nominal,digits = 3)) 
 print(confusion_matrix(y_test, predictions_nominal)) 
 
+print("\n------------------Future data-------------------------\n")
+
 print(result_train.predict(pd.DataFrame([[1.2, 1.1], 
                                    [1.5, -0.8]], 
                                   columns = ["Lag1","Lag2"])))
+
+print("\n----------------------------------------------------------------")
+print("\nLinear Discriminant Analysis LDA\n")
 
 
 
