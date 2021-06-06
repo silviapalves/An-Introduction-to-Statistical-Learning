769cfaf5c072bef23fa9a248305d8431cbb503aa
silviapalves <silviapalves@yahoo.com.br>
2021-06-06 09:51:59 -0300

    LDA

--- Smarket.py
+++ Smarket.py
@@ -2,8 +2,9 @@ import pandas as pd
 import statsmodels.api as sm
 import statsmodels.formula.api as smf
 import numpy as np
-from sklearn.metrics import confusion_matrix, classification_report
-
+from sklearn.metrics import confusion_matrix, classification_report, precision_score
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis, QuadraticDiscriminantAnalysis
+import matplotlib.pyplot as plt
 
 smarket = pd.read_csv(r"C:\Users\spalves\Desktop\Silvia\Pessoal\ITAU\ALL+CSV+FILES\ALL CSV FILES\Smarket.csv", index_col=0, parse_dates=True)
 smarket.dropna() 
@@ -76,7 +77,31 @@ print(result_train.predict(pd.DataFrame([[1.2, 1.1],
 print("\n----------------------------------------------------------------")
 print("\nLinear Discriminant Analysis LDA\n")
 
+X_train = smarket[:'2004'][['Lag1','Lag2']]
+y_train = smarket[:'2004']['Direction']
 
+X_test = smarket['2005':][['Lag1','Lag2']]
+y_test = smarket['2005':]['Direction']
+
+lda = LinearDiscriminantAnalysis()
+model = lda.fit(X_train, y_train)
+print("Prior probability to going down and up:")
+print(model.priors_)
+print("Average of each predictor used as estimate of mik (Lines: Down, UP; Columns: Lag1, Lag2):")
+print(model.means_)
+print("Coefficients of linear discriminants:")
+print(model.coef_)
+
+pred=model.predict(X_test)
+print(np.unique(pred, return_counts=True))
+
+print(confusion_matrix(pred, y_test))
+print(classification_report(y_test, pred, digits=3))
+probs_positive_class = model.predict_proba(X_test)[:, 1]
+# say default is the positive class and we want to make few false positives
+prediction = probs_positive_class > 0.9
+print("Number of downs with 90% of trheshould (posterior probability):")
+print(np.sum(prediction))
 
 
                              
