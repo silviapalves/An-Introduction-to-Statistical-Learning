45239645814032cc7df7f77f25eef769c6291a02
silviapalves <silviapalves@yahoo.com.br>
2021-06-09 19:03:01 -0300

    Classification Trees (without prunning)

---
+++ Carseats.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import graphviz
+import matplotlib.pyplot as plt
+from IPython.display import display
+
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor, DecisionTreeClassifier, export_graphviz
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.metrics import confusion_matrix, mean_squared_error
+
+carseats = pd.read_csv(r"C:\Users\spalves\Desktop\Silvia\Pessoal\ITAU\ALL+CSV+FILES\ALL CSV FILES\Carseats.csv")
+carseats['High'] = carseats.Sales.map(lambda x: 1 if x>8 else 0)
+carseats.ShelveLoc = pd.factorize(carseats.ShelveLoc)[0]
+carseats.Urban = carseats.Urban.map({'No':0, 'Yes':1})
+carseats.US = carseats.US.map({'No':0, 'Yes':1})
+carseats.info()
+
+X = carseats.drop(['Sales', 'High'], axis = 1)
+y = carseats.High
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size = 0.8, random_state = 0)
+
+classification_tree_carseats = DecisionTreeClassifier(max_depth = 6)
+classification_tree_carseats.fit(X_train, y_train)
+print("Training accurancy: \n"+ str(classification_tree_carseats.score(X_train, y_train)*100)+"%")
+
+export_graphviz(classification_tree_carseats, 
+                out_file = "carseat_tree.dot", 
+                feature_names = X_train.columns)
+
+with open("carseat_tree.dot") as f:
+    dot_graph = f.read()
+display(graphviz.Source(dot_graph))
+
+pred = classification_tree_carseats.predict(X_test)
+cm = pd.DataFrame(confusion_matrix(y_test, pred).T, 
+                  index = ['No', 'Yes'], 
+                  columns = ['No', 'Yes'])
+print(cm)
+print("\nCorrect prediction:\n")
+print(str((36+22)/80*100) + "%")
