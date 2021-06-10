4a3df86017a1c761e9bd396e12377db90701acab
silviapalves <silviapalves@yahoo.com.br>
2021-06-10 20:07:53 -0300

    SVC kernel = linear

--- SVC_and_SVM.py
+++ SVC_and_SVM.py
@@ -47,6 +47,10 @@ plt.ylabel('X2')
 # c large, then the margins will be narrow and there will be few 
 # support vectors on the margin or violating the margin.
 
+print("\n-----------------------------------------------\n")
+print("-------- Example non-linear separable ---------")
+print("\n-----------------------------------------------\n")
+
 print("\n-------- C = 1 --------\n")
 
 C = 1
@@ -75,4 +79,44 @@ tuned_parameters = [{'C': [0.001, 0.01, 0.1, 1, 5, 10, 100]}]
 clf = GridSearchCV(SVC(kernel='linear'), tuned_parameters, cv=10, scoring='accuracy')
 clf.fit(X, y)
 
-print(clf.cv_results_)
+clf_results = clf.cv_results_
+
+print("The best C resulted from the Cross Validation is: ")
+print(clf.best_params_)
+
+print("\n-------- Predicting with the best C --------\n")
+
+# only changing the random seed
+np.random.seed(1)
+X_test = np.random.randn(20,2)
+y_test = np.random.choice([-1,1], 20)
+X_test[y_test == 1] = X_test[y_test == 1]-1
+
+svc2 = SVC(C=0.001, kernel='linear')
+svc2.fit(X, y)
+y_pred = svc2.predict(X_test)
+ConfMat = pd.DataFrame(confusion_matrix(y_test, y_pred), index=svc2.classes_, columns=svc2.classes_)
+print(ConfMat)
+
+print("\n-----------------------------------------------\n")
+print("-------- Example linear separable ---------")
+print("\n-----------------------------------------------\n")
+
+X_test[y_test == 1] = X_test[y_test == 1]-1
+plt.figure()
+plt.scatter(X_test[:,0], X_test[:,1], s=70, c=y_test, cmap=mpl.cm.Paired)
+plt.xlabel('X1')
+plt.ylabel('X2')
+
+C =1e5
+svc3 = SVC(C=C, kernel='linear')
+svc3.fit(X_test, y_test)
+plot_svc(svc3, X_test, y_test,C)
+
+C =1
+svc4 = SVC(C=C, kernel='linear')
+svc4.fit(X_test, y_test)
+plot_svc(svc4, X_test, y_test,C)
+
+
+# C menor aumenta a margem o faz o test performar melhor
