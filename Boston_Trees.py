cfa69b1e657a5d17aa08f1b2f5c1b6591a781075
silviapalves <silviapalves@yahoo.com.br>
2021-06-10 08:30:31 -0300

    Finishing bagging, forest and boosting

--- Boston_Trees.py
+++ Boston_Trees.py
@@ -16,11 +16,15 @@ boston.dropna()
 
 X = boston.drop('medv', axis = 1)
 y = boston.medv
-X_train, X_test, y_train, y_test = train_test_split(X, y, train_size = 0.8, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size = 0.5, random_state = 0)
+
+print("\n--------Regression Tree--------\n")
 
 # Pruning not supported. Choosing max depth 2)
-regr_tree_boston = DecisionTreeRegressor(max_depth = 2)
+regr_tree_boston = DecisionTreeRegressor(max_depth = 3)
 regr_tree_boston.fit(X_train, y_train)
+print("Training accurancy: \n"+ str(regr_tree_boston.score(X_train, y_train)*100)+"%")
+
 
 export_graphviz(regr_tree_boston, 
                 out_file = "boston_tree.dot", 
@@ -53,6 +57,9 @@ print("\n--------Bagging--------\n")
 # Bagging: using all features
 bagged_boston = RandomForestRegressor(max_features = 13, random_state = 1)
 bagged_boston.fit(X_train, y_train)
+print("\nTraining MSE:\n")
+print(mean_squared_error(y_train, bagged_boston.predict(X_train)))
+print("\nTraining accurancy: \n"+ str(bagged_boston.score(X_train, y_train)*100)+"%")
 
 pred = bagged_boston.predict(X_test)
 
@@ -60,7 +67,7 @@ plt.figure()
 plt.scatter(pred, 
             y_test, 
             label = 'medv')
-
+plt.title("Bagging")
 plt.plot([0, 1], 
          [0, 1], 
          '--k', 
@@ -75,7 +82,7 @@ print(mean_squared_error(y_test, pred))
 print("\n--------Random Forest--------\n")
 
 # Random forests: using 6 features
-random_forest_boston = RandomForestRegressor(max_features = 11, random_state = 1)
+random_forest_boston = RandomForestRegressor(max_features = 6, random_state = 1)
 
 random_forest_boston.fit(X_train, y_train)
 
@@ -84,7 +91,8 @@ pred = random_forest_boston.predict(X_test)
 print("\nTest MSE:\n")
 print(mean_squared_error(y_test, pred))
 
-plt.figure() 
+plt.figure()
+plt.title("Random Forest") 
 plt.scatter(pred, 
             y_test, 
             label = 'medv')
@@ -107,3 +115,54 @@ Importance.sort_values(by = 'Importance',
 
 plt.xlabel('Variable Importance')
 plt.gca().legend_ = None
+
+print("\n--------Boosting--------\n")
+
+boosted_boston = GradientBoostingRegressor(n_estimators = 200, 
+                                           learning_rate = 0.01, 
+                                           max_depth = 4, 
+                                           random_state = 1)
+
+boosted_boston.fit(X_train, y_train)
+
+feature_importance = boosted_boston.feature_importances_*100
+
+rel_imp = pd.Series(feature_importance, 
+                    index = X.columns).sort_values(inplace = False)
+
+rel_imp.T.plot(kind = 'barh', 
+               color = 'r', )
+
+plt.xlabel('Variable Importance')
+
+plt.gca().legend_ = None
+
+
+print("\nTest MSE:\n")
+print(mean_squared_error(y_test, boosted_boston.predict(X_test)))
+
+plt.figure() 
+plt.title("Boosting") 
+plt.scatter(boosted_boston.predict(X_test), 
+            y_test, 
+            label = 'medv')
+
+plt.plot([0, 1], 
+         [0, 1], 
+         '--k', 
+         transform = plt.gca().transAxes)
+
+plt.xlabel('pred')
+plt.ylabel('y_test')
+
+# n_estimators = number of trees
+boosted_boston2 = GradientBoostingRegressor(n_estimators = 5000, 
+                                            learning_rate = 0.2, 
+                                            max_depth = 4, 
+                                            random_state = 1)
+boosted_boston2.fit(X_train, y_train)
+
+mean_squared_error(y_test, boosted_boston2.predict(X_test))
+
+print("\nTest MSE:\n")
+print(mean_squared_error(y_test, boosted_boston2.predict(X_test)))
